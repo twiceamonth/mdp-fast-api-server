6f68237595d0f2f6b8e9b7a9c6a5b04124a6a80b
Maxim Vasenin <m.vasenin@bk.ru>
2024-11-16 15:26:13 +0700

    - добавлены ендпоинты для position

---
+++ src/app/views/position/crud.py
@@ -0,0 +1,46 @@
+from fastapi import HTTPException
+from sqlalchemy import select, Result
+from sqlalchemy.orm import Session
+from starlette.status import HTTP_404_NOT_FOUND
+
+from app.db.base import convert_to_db
+from app.db.models.position import PositionDTO
+from app.views.position.model import Position, PositionResponse
+
+
+def get_positions(session: Session) -> list[PositionResponse]:
+    q = select(PositionDTO).order_by(PositionDTO.name)
+    result: Result = session.execute(q)
+    positions = result.scalars().all()
+    return [PositionResponse.model_validate(position) for position in positions]
+
+def create_new_position(session: Session,new_position: Position):
+    sao = convert_to_db(new_position, PositionDTO)
+    session.add(sao)
+    session.commit()
+    session.refresh(sao)
+    return sao
+
+def delete_position_by_id(session: Session, position_id: str):
+    position = session.get(PositionDTO, position_id)
+    if position is not None:
+        session.delete(position)
+        session.commit()
+        return { "message" : "Success" }
+    raise HTTPException(
+        status_code=HTTP_404_NOT_FOUND,
+        detail=f"Должности с id {position_id} не найдено!",
+    )
+
+def update_position_patch(session: Session, position_id: str, new_position: Position) -> PositionResponse:
+    new_position.model_dump(exclude_unset=True)
+    old_position = session.get(PositionDTO, position_id)
+    if old_position is not None:
+        for name, value in new_position.model_dump(exclude_unset=True).items():
+            setattr(old_position, name, value)
+        session.commit()
+        return old_position
+    raise HTTPException(
+        status_code=HTTP_404_NOT_FOUND,
+        detail=f"Должности с id {position_id} не найдено!",
+    )
--- src/app/views/position/position_views.py
+++ src/app/views/position/position_views.py
@@ -2,22 +2,28 @@ from typing import List
 
 from fastapi import APIRouter
 
+from app.views.position.crud import create_new_position, get_positions, delete_position_by_id, update_position_patch
 from src.app.views.position.model import PositionResponse, Position
+from src.app.db.db_connection import session as db
 
 router = APIRouter(tags=["Должности"])
 
-@router.get("/positions")
-def get_positions_list() -> List[PositionResponse]:
-    return []
 
-@router.post("/position")
+@router.get("/positions", response_model=List[PositionResponse])
+def get_positions_list() -> List[PositionResponse]:
+    return get_positions(db)
+
+
+@router.post("/position", response_model=PositionResponse)
 def create_position(position: Position):
-    return {}
+    return create_new_position(db, position)
+
 
 @router.delete("/position/{position_id}")
 def delete_position(position_id: str):
-    return []
+    return delete_position_by_id(db, position_id)
 
-@router.patch("/position/{position_id}")
+
+@router.patch("/position/{position_id}", response_model=PositionResponse)
 def update_position(position_id: str, new_position: Position):
-    return {}
+    return update_position_patch(db, position_id, new_position)
